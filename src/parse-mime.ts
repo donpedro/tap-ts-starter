a7791afacd20002239371b64a0a0e0b6e6497c41
Peter Lambert <peterl@sbcotton.com>
2018-05-17 22:35:27 -0500

    Update packages

--- src/parse-mime.ts
+++ src/parse-mime.ts
@@ -1,7 +1,11 @@
-/** Parse MIME files using [Nodemailer.Mailparser](https://nodemailer.com/extras/mailparser/) 
- * 
-*/ /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
+/** Parse MIME files using [Nodemailer.Mailparser](https://nodemailer.com/extras/mailparser/)
+ *
+ */ /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
 
+/* 
+Updated mailparser to 2.2.0 to avoid malicious getcookies module; see https://blog.npmjs.org/post/173526807575/reported-malicious-module-getcookies
+Consider mailparse (https://github.com/javascriptlove/mailparse) for the future, since mailparser will no longer be maintained. Mailparse does not yet have any TypeScript types available.
+*/
 import * as mailparser from 'mailparser'
 //var mp = mailparser.MailParser; // low-level parser
 var sp = mailparser.simpleParser // higher-level parser (easier to use, not as efficient)
