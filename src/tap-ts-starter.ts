08c42483cb7425371e553d40511b28a9652b34ad
Jonathan <jonathan@sbcotton.com>
2018-05-25 13:04:25 -0500

    console display async await conversion

--- src/tap-ts-starter.ts
+++ src/tap-ts-starter.ts
@@ -3,9 +3,7 @@
 // ...
 export default class DummyClass {}
 
-console.log(
-  'working!'
-) /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
+console.log('working!') /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
 
 /**
  * This module is the entry point for local execution as a Singer tap (see the [spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md))
@@ -17,16 +15,19 @@ import * as scanDir from './scan-dir'
 /** random note */
 
 /** random note 2
-  * 
-  */
-configLoader
-  .loadConfig()
-  .then(function(configObjs) {
-    // run scanDir using parseMime as the parser for each item
+ *
+ */
+var globalClass = async () => {
+  try {
+    var configObjs = await configLoader.loadConfig()
     return scanDir.scanDir(configObjs, parseMime.parseItem)
-  })
-  .catch(function(error) {
-    // Handle errors
-    console.error('Error: ', error)
-    return error
-  })
+  } catch {
+    var error = (error: any) => {
+      // Handle errors
+      console.error('Error: ', error)
+      return error
+    }
+  }
+}
+
+globalClass()
