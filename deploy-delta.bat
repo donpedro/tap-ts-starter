6f4d95be4a81b911846c94844ca19ea9edb96ed4
Peter Lambert <peterl@sbcotton.com>
2018-06-23 00:56:40 -0500

    serverless.yml now deploys a test bucket for holding DemoTest user tests

---
+++ deploy-delta.bat
@@ -0,0 +1,8 @@
+echo off
+echo This will deploy to the Delta account
+echo AWS profile "delta" will be used, and if it doesn't exist the operation will fail.
+echo For Windows, check profiles in C:\Users\(username)\.aws
+pause
+echo on
+call serverless deploy --prefix delta --serviceName tap-starter --profile delta --accountId 540471531016 --verbose
+pause
