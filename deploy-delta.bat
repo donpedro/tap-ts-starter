8dcbb7f36654dcac590e25f5728743c8721057e5
Peter Lambert <peterl@sbcotton.com>
2018-07-04 15:58:10 -0500

    deploy-delta.bat bug fix

--- deploy-delta.bat
+++ deploy-delta.bat
@@ -4,5 +4,5 @@ echo AWS profile "delta" will be used, and if it doesn't exist the operation wil
 echo For Windows, check profiles in C:\Users\(username)\.aws
 pause
 echo on
-call serverless deploy --profile delta --prefix delta --accountId 540471531016 --verbose
+call serverless deploy --aws-profile delta --prefix delta --accountId 540471531016 --verbose
 pause
